fddf1fc9b9104eff31e3e400f0fc762b54544b3f
方争由 <fangzhengyou@fangzhengyoudeMacBook-Pro.local>
2016-09-30 10:22:04 +0800

    Add DotProgressView to this project

--- app/src/main/java/com/digital/customview/MainActivity.java
+++ app/src/main/java/com/digital/customview/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.digital.customview.view.DotProgressView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ((DotProgressView)findViewById(R.id.dot1)).startDelay(120);
+        ((DotProgressView)findViewById(R.id.dot2)).startDelay(360);
+        ((DotProgressView)findViewById(R.id.dot3)).startDelay(640);
     }
 
     public void onClick(View view){
