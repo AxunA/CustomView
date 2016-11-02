cd9b20de9c9035ffedcd4459ff2cc69424adf4f7
方争由 <fangzhengyou@fangzhengyoudeMacBook-Pro.local>
2016-11-02 14:27:17 +0800

    add activity annimation

--- app/src/main/java/com/digital/customview/MainActivity.java
+++ app/src/main/java/com/digital/customview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.digital.customview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity implements DigitalBannerView.BannerItemClickListener{
+public class MainActivity extends AppCompatActivity implements DigitalBannerView.BannerItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,34 +23,37 @@ public class MainActivity extends AppCompatActivity implements DigitalBannerView
         setContentView(R.layout.activity_main);
 
         //init DotProgress
-        ((DotProgressView)findViewById(R.id.dot1)).startDelay(120);
-        ((DotProgressView)findViewById(R.id.dot2)).startDelay(360);
-        ((DotProgressView)findViewById(R.id.dot3)).startDelay(640);
+        ((DotProgressView) findViewById(R.id.dot1)).startDelay(120);
+        ((DotProgressView) findViewById(R.id.dot2)).startDelay(360);
+        ((DotProgressView) findViewById(R.id.dot3)).startDelay(640);
 
         //init VerticalViewPager
-        String [] array={TestData.STRING_1,TestData.STRING_2,TestData.STRING_3};
+        String[] array = {TestData.STRING_1, TestData.STRING_2, TestData.STRING_3};
         VerticalPagerAdapter adapter = new VerticalPagerAdapter(this, array);
-        VerticalViewPager verticalViewPager=(VerticalViewPager)findViewById(R.id.vvp_broadcast);
+        VerticalViewPager verticalViewPager = (VerticalViewPager) findViewById(R.id.vvp_broadcast);
         verticalViewPager.setAdapter(adapter);
 
         //init DigitalBannerView
-        List<Integer> mImageAddressList=new ArrayList<>();
+        List<Integer> mImageAddressList = new ArrayList<>();
         mImageAddressList.add(R.drawable.banner_1);
         mImageAddressList.add(R.drawable.banner_2);
-        DigitalBannerView bannerView=(DigitalBannerView)findViewById(R.id.bv_img);
-        bannerView.initPageView(mImageAddressList,this);
+        DigitalBannerView bannerView = (DigitalBannerView) findViewById(R.id.bv_img);
+        bannerView.initPageView(mImageAddressList, this);
     }
 
-    public void onClick(View view){
-        switch (view.getId()){
+    public void onClick(View view) {
+        switch (view.getId()) {
             case R.id.dci_ac_info:
-                    showToast(getString(R.string.account_info));
+                showToast(getString(R.string.account_info));
+                Intent intent = new Intent(this, TestActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.activity_open_in, R.anim.activity_open_out);
                 break;
         }
     }
 
-    private void showToast(String str){
-        Toast.makeText(this,str,Toast.LENGTH_SHORT).show();
+    private void showToast(String str) {
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
     }
 
     @Override
